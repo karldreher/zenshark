ab3c4704ce1f3c8ddc54def5c4ea7ab6039967d2
Karl Dreher <karldreher@outlook.com>
2021-10-17 10:51:26 -0700

    React version

---
+++ src/components/table.js
@@ -0,0 +1,14 @@
+function Table(){
+    return(
+        <div>
+            <label for="find-game">Find a game:  </label>
+            <input type="text" id="find-game"></input>
+            
+            <table id="table">
+
+            </table>
+        </div>
+    )
+}
+
+export default Table
